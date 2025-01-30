a615ae68d56ee2c1aed86ac1974cbb2c1db6a9e8
burichel <deivson.burichel@gmail.com>
2025-01-30 04:28:16 +0000

    VERSÃO FINAL

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,71 +1,71 @@
 #include <stdio.h>
 // DESAFIO SUPER TRUNFO.
 
-int main (){
+int main() {
 
-// CADASTRO DAS VARÍAVEIS.
-char Estado [20] = "indefinido"; 
-char Código [20] = "indefinido";
-char Cidade [20] = "indefinido";
-float população;
-float Área;
-float Pib;
-int PontosTuristicos;
-char sim  = 's';
+// CADASTRO DAS VARIÁVEIS.
+    char estado [] = "estado";
+    char codigo [] = "codigo";
+    char cidade [] = "cidade";
+    float populacao;
+    float area;
+    float pib;
+    int PontosTuristicos;
+    char sim = 's';
 
-// APRESENTAÇÃO DAS INSTRUÇÕES DE USO.
-printf(" *****     INTRODUÇÃO     *****\n");
-printf("A) - A partir de agora você irá criar a sua carta super trunfo com base em informações de cidades;\n");
-printf("B) - Siga as instruções e responda as perguntas obedecendo os comandos;\n");
-printf("C) - O sistema ainda não reconhece palavras compostas Ex. São Paulo, São Felix, etc., Sendo assim inclua apenas palavras simples.\n");
+// APRESENTAÇÃO DO PROGRAMA COM INSTRUÇÕES DE USO.
+    printf("*****     INTRODUÇÃO     *****\n");
+    printf("A) - Neste programa você irá criar a sua carta SUPER TRUNFO com base nas  informações de cidades.\n;");
+    printf("B) - Siga as instruções e responda as perguntas obedecendo os comandos.;\n");
+    printf("C) - ATENÇÃO, o programa ainda não reconhece palavras compostas ex. São Paulo, Rio de Janeiro, etc. Inclua apenas palavras simples.\n");
 
-// PERGUNTA DE CONFIRMAÇÃO DE USO.
-printf("PRONTO PRA COMEÇAR? DIGITE s = (sim)\n");
-scanf("%c", &sim);
+// PERGUNTA DE INTERAÇÃO.
+    printf("VAMOS COMEÇAR? DIGITE s PARA  = (SIM)\n");
+    scanf("%c", &sim);
 
-// ATRIBUIÇAO DO NOME DO ESTADO.
-printf("DIGITE O NOME DO ESTADO:\n");
-scanf("%s", &Estado);
+// ATRIBUIÇÃO DO NOME DO ESTADO.
+    printf("DIGITE O NOME DO ESTADO:\n");
+    scanf("%s", &estado);
 
-// ATRIBUIÇAO DO CÓDIGO.
-printf("DIGITE O CÓDIGO DO ESTADO EX.: (AS 02 PRIMEIRAS LETRAS + O NUMERAL SP01, SP02...):\n");
-scanf("%s", &Código);
+// ATRIBUIÇÃO DO CÓDIGO.
+    printf("DIGITE O CÓDIGO DO ESTADO EX,: (AS 2 PRIMEIRAS LETRAS DO ESTADO + NUMERAL = SP01, SP 02, RJ01, RJ02...):\n");
+    scanf("%s", &codigo);
 
 // ATRIBUIÇAO DO NOME DA CIDADE.
-printf("DIGITE O NOME DA CIDADE:\n");
-scanf("%s", &Cidade);
+    printf("DIGITE O NOME DA CIDADE:\n");
+    scanf("%s", &cidade);
+    
+// ATRIBUIÇÃO DA POPULAÇÃO.    
+    printf("DIGITE A POPULAÇÃO SEM PONTO (.) E SEM VÍRGULA (,) EX.: (2000 / 20000 / 200000 ...):\n");
+    scanf("%f", &populacao);
 
-// ATRIBUIÇAO DA POPULAÇÃO.
-printf("DIGITE A POPULAÇÃO SEM PONTO/VÍRGULAS (./,) EX.: (2035640001, 10360335...):\n");
-scanf("%f", &população);
+// ATRIBUIÇÃO DO TAMANHO DA ÁREA TERRITORIAL.
+    printf("DIGITE O TAMANHO DA ÁREA EM KM² EX.: (42.000 / 555.000 OBS.: NÃO COLOCAR VÍRGULA (,)):\n");
+    scanf("%f", &area);
 
-// ATRIBUIÇAO TO TAMANHO DA ÁREA TERRITORIAL. 
-printf("DIGITE O TAMANHO DA ÁREA EM KM² EX.: (42.000, 420.000 OBS.: NÃO COLOCAR VÍRGULA (,)):\n");
-scanf("%f", &Área);
+// ATRIBUIÇÃO DO PIB.
+    printf("DIGITE O VALOR DO PIB EX.: (123.456 / 56.789):\n");
+    scanf("%f", &pib);
 
-// ATRIBUIÇAO DO VALOR DO PIB.
-printf("DIGITE O VALOR DO PIB EX.: (54.970):\n");
-scanf("%f", &Pib);
+// ATRIBUIÇÃO DO NÚMERO DE PONTOS TURÍSTICOS
+    printf("DIGITE O NÚMERO DE PONTOS TURÍSTICOS:\n");
+    scanf("%d", &PontosTuristicos);
 
-// ATRIBUIÇAO DA QUANTIDADE DE PONTOS TURÍSTICOS.
-printf("DIGITE A QUANTIDADE DE PONTOS TURÍSTICOS:\n");
-scanf("%d", &PontosTuristicos);
-
-//IMPRIME O CABEÇALHO: DADOS DA CARTA.
-printf(" *****  DADOS DA CARTA  *****\n");
-printf(" ----------------------------\n");
-
-
+// IMPRIME O CABEÇALHO - DADOS DA CARTA -.
+    printf("*****     DADOS DA CARTA     *****\n");
+    printf("----------------------------------\n");
 
 // APRESENTA OS DADOS DA CARTA CADASTRADA.
-printf("ESTADO: %s\n", Estado);
-printf("CÓDIGO DA CIDADE: %s\n", Código);
-printf("CIDADE: %s\n", Cidade);
-printf("POPULAÇÃO: %f\n", população);
-printf("ÁREA EM KM²: %f\n", Área);
-printf("PIB: %f\n", Pib);
-printf("Nº PONTO TURÍSTICOS: %d\n", PontosTuristicos);
+    printf("ESTADO: %s\n", estado);
+    printf("CÓDIGO: %s\n", codigo);
+    printf("CIDADE: %s\n", cidade);
+    printf("POPULAÇÃO: %f\n", populacao);
+    printf("ÁREA EM KM²: %f\n", area);
+    printf("PIB: %f\n", pib);
+    printf("Nº DE PONTOS TURÍSTICOS: %d", PontosTuristicos);
+
 
 return 0;
+
 
 }
